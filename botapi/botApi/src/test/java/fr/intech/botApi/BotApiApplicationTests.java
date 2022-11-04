15ca442c71afe3a9176e0cc6a5b5a95ba2299a66
DEVRED Paul <devred@et.intechinfo.fr>
2022-11-04 14:07:42 +0100

    copie de fichiers

---
+++ botapi/botApi/src/test/java/fr/intech/botApi/BotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.intech.botApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
