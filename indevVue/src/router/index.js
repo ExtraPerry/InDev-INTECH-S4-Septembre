0e327920841dd853e5ee2995f32f3de9cabba3c7
rpalmier <rpalmier@et.intechinfo.fr>
2022-11-25 09:49:49 +0100

    update

--- indevVue/src/router/index.js
+++ indevVue/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import adminView from '../views/adminView.vue';
+import adminView from '../views/AdminView.vue';
 
 
 const router = createRouter({
