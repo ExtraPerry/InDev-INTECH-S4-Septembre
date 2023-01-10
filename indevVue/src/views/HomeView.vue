9b79f577e338b16839811564810a58b858b1c068
DEVRED Paul <devred@et.intechinfo.fr>
2023-01-10 14:21:59 +0100

    avancement css

--- indevVue/src/views/HomeView.vue
+++ indevVue/src/views/HomeView.vue
@@ -8,6 +8,9 @@
     <div class="cards"><p>je suis une carte</p></div>
     <div class="cards"><p>je suis une carte</p></div>
     <div class="cards"><p>je suis une carte</p></div>
+    <div class="cards"><p>je suis une carte</p></div>
+    <div class="cards"><p>je suis une carte</p></div>
+    <div class="cards"><p>je suis une carte</p></div>
   </div>
 </div>
  
