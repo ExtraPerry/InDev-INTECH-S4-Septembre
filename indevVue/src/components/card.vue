b70310f5eb27ae9fed9f7e47c1752dd61b9f67a8
DEVRED Paul <devred@et.intechinfo.fr>
2023-01-11 19:16:51 +0100

    premiere carte reactive

--- indevVue/src/components/card.vue
+++ indevVue/src/components/card.vue
@@ -1,19 +1,41 @@
 <script setup>
+
+    const props = defineProps({
+        siteTitle: String,
+        title: String,
+        description: String,
+        tags: Array
+    })
+
 </script>
 
 <template>
     <div class="card">
-        <div class="siteTitle">Titre</div>
-        <div class="title">tire discord</div>
-        <div class="description">description</div>
-        <div class="tags">liste de tags</div>
+        <div class="imgContainer">
+            <img src="../../ressources/Notion-logo.svg.png" alt="">
+        </div>
+        <div class="infoContainer">
+        <p class="siteTitle">{{siteTitle}}</p>
+        <p class="title">{{title}}</p>
+        <p class="description">{{description}}</p>
+        <p class="tags">{{tags}}</p>
+        </div>
+      
     </div>
 
 </template>
 
 <style scoped>
+
+    @import url('https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=League+Gothic&family=Lobster&family=Prompt:wght@300&family=Roboto:wght@100&display=swap');
+
+
+
    .card{
     display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-indent: 0.1em;
     background-color: #36393f ;
     width: 22em;
     height: 9em;
@@ -23,6 +45,22 @@
     }
     .card .siteTitle {
         font-weight: bold;
+        /* margin: 1em; */
+    }
+    .card p {
+        margin: 0.1em;
+    }
+    .card .description {
+        font-weight: 100;
+    }
+    .card img {
+        width: 5em;
+        height: 5em;
+    }
+    .imgContainer {
+        display: flex;
+        align-items: center;
+        margin: 1em;
     }
 
 </style>
--- indevVue/src/views/HomeView.vue
+++ indevVue/src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <script setup>
 import card from '../components/card.vue';
+import { ref } from 'vue';
+
+const siteTitle = ref('Titre');
+
 </script>
 
 <template>
 <div class="home">
   <div class="wall">
+    <card :siteTitle= siteTitle />
     <card></card>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
-    <div class="cards"><p>je suis une carte</p></div>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    <card></card>
+    
   </div>
 </div>
  
