3a7a1557986bb9d175afe766b9b57d6ddccec2fb
DEVRED Paul <devred@et.intechinfo.fr>
2023-01-11 11:00:49 +0100

    ajout du composant card, non fonctionnel

---
+++ indevVue/src/components/card.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+    <div class="card">
+        <div class="siteTitle">Titre</div>
+        <div class="title">tire discord</div>
+        <div class="description">description</div>
+        <div class="tags">liste de tags</div>
+    </div>
+
+</template>
+
+<style scoped>
+   .card{
+    display: flex;
+    background-color: #36393f ;
+    width: 22em;
+    height: 9em;
+    margin: 0.5em;
+    border-radius: 10px;
+
+    }
+    .card .siteTitle {
+        font-weight: bold;
+    }
+
+</style>
--- indevVue/src/views/HomeView.vue
+++ indevVue/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import card from '../components/card.vue';
 </script>
 
 <template>
 <div class="home">
   <div class="wall">
+    <card></card>
     <div class="cards"><p>je suis une carte</p></div>
     <div class="cards"><p>je suis une carte</p></div>
     <div class="cards"><p>je suis une carte</p></div>
