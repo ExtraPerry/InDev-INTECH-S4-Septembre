deebb0e3a23fed67a25991a9ac2ba6a8900ea9d2
Gervais Pierre <94633856+GervaisPierre@users.noreply.github.com>
2023-01-16 17:20:45 +0100

    Switching to desktop

--- BotApi/src/main/java/com/example/BotApi/controller/SpaController.java
+++ BotApi/src/main/java/com/example/BotApi/controller/SpaController.java
@@ -1,6 +1,6 @@
 package com.example.BotApi.controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BotApi.model.Category;
@@ -35,13 +36,27 @@ public class SpaController {
 	
 	//Endpoints.
 	@GetMapping("/getItemsPage")	//This will be used to get an infinite scrolling page by requesting it in portions at a time.
-	public Page<Item> getItemPage(@RequestBody final PageFormat pageFormat){
+	public Object getItemPage(@RequestParam final Map<String, String> params){
+		//Check if parameters are correct.
+		if (!(params.containsKey("page") && params.containsKey("size") && params.containsKey("sort"))) {
+			return "Parameters are not valid.";
+		}
+		//Check & Convert to an integer the String value of the page & size parameter.
+		int page;
+		int size;
+		try {
+			page = Integer.parseInt(params.get("page"));
+			size = Integer.parseInt(params.get("size"));
+		}catch(NumberFormatException error) {
+			return "Page or Size was not a valid number.";
+		}
+		
 		Pageable sortedBy = PageRequest.of(pageFormat.getPage(), pageFormat.getSize(), Sort.by(pageFormat.getSort()));
 		return this.getItemRepo().findAll(sortedBy);
 	}
 	@GetMapping("/getTagsPage")		//This will help get a proposition of tags when trying to search for a specific tag inside of a search bar.
-	public Page<Tag> getTags(@RequestBody final PageFormat pageFormat){
-		Pageable sortedBy = PageRequest.of(pageFormat.getPage(), pageFormat.getSize(), Sort.by(pageFormat.getSort()));
+	public Page<Tag> getTags(@RequestParam final Map<String, String> params){
+		Pageable sortedBy = PageRequest.of();
 		return this.getTagRepo().findAll(sortedBy);
 	}
 	@GetMapping("/getTagsById")		//This will be used to get a specific tag
