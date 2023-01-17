a07ed1b33b9e76cbfc9c559ed2417b427df1e649
DEVRED Paul <devred@et.intechinfo.fr>
2023-01-17 18:15:52 +0100

    avant fetch data

--- indevVue/src/views/HomeView.vue
+++ indevVue/src/views/HomeView.vue
@@ -3,6 +3,22 @@ import card from '../components/card.vue';
 import { ref } from 'vue';
 
 const siteTitle = ref('Titre');
+const initialItems = ref([]);
+
+async function getInitialItems() {
+    const res = await fetch("http://localhost:8080/getItemsPage", {
+      body: {
+      'page':0,
+      'size':10,
+      'sort':'id'
+      }
+    });
+    const finalRes = await res.json();
+    initialItems.value = finalRes;
+  }
+
+  getInitialItems();
+  console.log(initialItems);
 
 </script>
 
