ea3226f252d22d01ea0db2008ba9b2a3946a6243
vanot <vanot@et.intechinfo.fr>
2023-01-25 09:51:04 +0100

    Database init files

---
+++ postgresql/data.sql
@@ -0,0 +1,32 @@
+/*
+
+INSERT INTO category (name) VALUES ();
+INSERT INTO discorduser (name) VALUES ();
+INSERT INTO item (name) VALUES ();
+INSERT INTO tag (name) VALUES ();
+
+CREATE TABLE category (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE discorduser (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE item (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tag (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+*/
